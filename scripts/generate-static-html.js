2a7fd34d0c8b3170b53c763c0f1258730aba3936
waledhanashri11 <dhanashriwale2005@gmail.com>
2026-09-07 17:10:36 +0530

    reviews

--- scripts/generate-static-html.js
+++ scripts/generate-static-html.js
@@ -27,6 +27,8 @@ const routes = [
   { path: "/terms", output: "../dist/client/terms/index.html" },
   { path: "/refund-policy", output: "../dist/client/refund-policy/index.html" },
   { path: "/shipping-policy", output: "../dist/client/shipping-policy/index.html" },
+  { path: "/recognition-awards", output: "../dist/client/recognition-awards/index.html" },
+  { path: "/recognization-awards", output: "../dist/client/recognization-awards/index.html" },
 ];
 
 async function generateStaticHtml() {
